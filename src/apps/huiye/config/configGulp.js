2307c6ad7b88247edfc9fe3902c0470158c71aa8
Peng Du <mutuyueniao@163.com>
2017-09-13 10:40:07 +0800

    huiye : finished

---
+++ src/apps/huiye/config/configGulp.js
@@ -0,0 +1,106 @@
+var appFolderName = 'huiye/';
+var buildFolderName = 'build/';
+var assetFolderName = '';
+var fontFolderName = 'fonts/';
+var imgFolderName = 'images/';
+var styleFolderName = 'css/';
+var scriptFolderName = 'js/';
+var templateFolderName = '';
+
+var configGulp = {
+
+    statics : [
+        {
+            name : 'jquery 1.12.3',
+            src : 'bower_components/jquery-1.12.3/dist/jquery.min.js',
+            dest : buildFolderName + appFolderName + assetFolderName + scriptFolderName,
+            watchable : false
+        },
+        {
+            name : 'font-awesome font files',
+            src : 'bower_components/font-awesome/fonts/**/*',
+            dest : buildFolderName + appFolderName + assetFolderName + fontFolderName,
+            watchable : false
+        },
+        {
+            name : 'font-awesome css files',
+            src : 'bower_components/font-awesome/css/font-awesome.min.css',
+            dest : buildFolderName + appFolderName + assetFolderName + styleFolderName,
+            watchable : false
+        },
+        {
+            name : 'owl.carousel2 js',
+            src : 'src/libs/owl-carousel-2/owl.carousel.min.js',
+            dest : buildFolderName + appFolderName + assetFolderName + scriptFolderName,
+            watchable : false
+        },
+        {
+            name : 'owl.carousel2 css files',
+            src : [
+                'src/libs/owl-carousel-2/assets/owl.carousel.min.css',
+                'src/libs/owl-carousel-2/assets/owl.theme.default.min.css'
+            ],
+            dest : buildFolderName + appFolderName + assetFolderName + styleFolderName,
+            watchable : false
+        },
+        {
+            name : 'webapp statics',
+            src : 'src/apps/' + appFolderName + 'static/**/*',
+            dest : buildFolderName + appFolderName + assetFolderName,
+            watchable : true
+        }
+    ],
+
+    styles : [
+        {
+            name : 'main styles',
+            src : 'src/apps/' + appFolderName + 'webapp/stylus/index.styl',
+            dest : buildFolderName + appFolderName + assetFolderName + styleFolderName,
+            styletype : 'stylus', //css, stylus, less, sass ...
+            concatname : 'index.css',
+            compressable : false,
+            watchable : true,
+            watchsrc : 'src/apps/' + appFolderName +'**/*.styl'
+        }
+    ],
+
+    scripts : [
+        {
+            name : 'main js',
+            src : 'src/apps/' + appFolderName +'webapp/js/app.js',
+            dest : buildFolderName + appFolderName + assetFolderName + scriptFolderName,
+            scripttype : 'js',
+            bundlename : 'app.js',
+            browserify : true,
+            compressable : false,
+            watchable : true
+        }
+    ],
+
+    templates : [
+        {
+            name : 'webapp templates',
+            src : [
+                'src/apps/' + appFolderName + 'webapp/**/*.jade',
+                '!src/apps/' + appFolderName + 'webapp/layout.jade'
+            ],
+            dest : buildFolderName + appFolderName + templateFolderName,
+            templatetype : 'jade', //html, jade, handlebar ...
+            watchable : true,
+            watchsrc : [
+                'src/apps/' + appFolderName + 'webapp/**/*.jade',
+                'src/apps/' + appFolderName + 'components/**/*.jade'
+            ]
+        }
+    ],
+
+    server : {
+        appport : 8081,
+        lrport : 35731,
+        staticfolder : buildFolderName + appFolderName
+    },
+
+    buildNum : 5 //本项目共有statics、styles、templates、scripts、server 五个构建任务
+}
+
+module.exports = configGulp;
---
+++ src/apps/huiye/webapp/js/app.js
@@ -0,0 +1,22 @@
+/* ==================================================================================
+ * app.js
+ * 主程序入口
+ * ================================================================================== */
+
+var Popbox = require('../../../../libs/dolphin/js/popbox');
+var AbsCenter = require('../../../../libs/dolphin/js/absCenter');
+
+var Webapp = (function(){
+
+    function Webapp(){
+        popbox = new Popbox($);
+        absCenter = new AbsCenter($);
+    }
+
+    return Webapp;
+
+})();
+
+$(document).ready(function(){
+    var app = new Webapp();
+});
